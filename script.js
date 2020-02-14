5e1bf6db99fecd05910bc590b17b4e5568cce25c
Nellie <fabybacenelson@gmail.com>
2020-02-14 15:40:48 -0700

    test on mobile

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ $(document).ready(function(){
 
     $("#heart").hover(function(){
         $("#heart").attr("fill","url(#cats2)");
+        $("#message2020").attr("style", "display: block;");
     }, function(){
         $("#heart").attr("fill","url(#cats1)");
+        $("#message2020").attr("style", "display: none;");
+    });
+
+    $("#heart").click(function(){
+        $("#heart").attr("fill","url(#cats2)");
+        $("#message2020").attr("style", "display: block;");
+    });
+    $("#heart").focusout(function(){
+        $("#heart").attr("fill","url(#cats1)");
+        $("#message2020").attr("style", "display: none;");
     });
 
 });
