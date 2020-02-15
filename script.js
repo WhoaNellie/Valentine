6a39e5a1f7a053ab80636511eac5e8c6ac5b3bd0
Nellie <fabybacenelson@gmail.com>
2020-02-14 17:00:38 -0700

    able to click on mobile

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("#heart").hover(function(){
-        $("#heart").attr("fill","url(#cats2)");
+    $("#heart").hover(function () {
+        $("#heart").attr("fill", "url(#cats2)");
         $("#message2020").attr("style", "display: block;");
-    }, function(){
-        $("#heart").attr("fill","url(#cats1)");
+    }, function () {
+        $("#heart").attr("fill", "url(#cats1)");
         $("#message2020").attr("style", "display: none;");
     });
 
-    $("#heart").click(function(){
-        $("#heart").attr("fill","url(#cats2)");
-        $("#message2020").attr("style", "display: block;");
-    });
-    $("#heart").focusout(function(){
-        $("#heart").attr("fill","url(#cats1)");
-        $("#message2020").attr("style", "display: none;");
+    // $("#heart").click(function () {
+    //     console.log("heart");
+    //     $("#heart").attr("fill", "url(#cats2)");
+    //     $("#message2020").attr("style", "left: 5%;");
+    // });
+    // $("#heart").focusout(function () {
+    //     $("#heart").attr("fill", "url(#cats1)");
+    //     $("#message2020").attr("style", "left: -9999px;");
+    // });
+
+    $(".v2020").click(function(){
+        if($("#heart").attr("fill") == "url(#cats1)"){
+            $("#heart").attr("fill","url(#cats2)");
+            $("#message2020").attr("style", "display: block;");
+        }else if($("#heart").attr("fill") == "url(#cats2)"){
+            $("#heart").attr("fill","url(#cats1)");
+            $("#message2020").attr("style", "display: none;");
+        }
     });
 
 });
